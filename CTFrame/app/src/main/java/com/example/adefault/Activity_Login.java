29829f2a805411f24af9bc79bb42b2c24544502f
OkSeungBae <osb1808@nate.com>
2019-05-05 15:09:57 +0900

    [SeungBae] activity_login (@drawable에 ctframe_button background생성해서 적용) , activity_signup background에 사진 적용 및 각종 object들 backgroud적용

--- CTFrame/app/src/main/java/com/example/adefault/Activity_Login.java
+++ CTFrame/app/src/main/java/com/example/adefault/Activity_Login.java
@@ -10,10 +10,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Activity_Login extends AppCompatActivity {
-    Button login_Btn;
-    TextView forgot_pw_Tv, sign_up_Tv;
-    Button btn_sign_in;
-    EditText signin_name, signin_password;
+    Button btn_login;
+    TextView tv_sign_up;
+    EditText edt_email, edt_password;
 
     String Email;
     String Password;
@@ -23,54 +22,16 @@ public class Activity_Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //forgot_pw_Tv = (TextView)findViewById(R.id.forgot_pw_Tv);
-        //sign_up_Tv = (TextView)findViewById(R.id.sign_up_Tv);
-        //login_Btn =(Button)findViewById(R.id.login_Btn);
+        tv_sign_up = (TextView)findViewById(R.id.tv_sign_up);
+        btn_login =(Button)findViewById(R.id.btn_login);
 
-        /*//로그인 동작
-        ((Button) login_Btn).setOnClickListener(new View.OnClickListener() {
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent intent = new Intent( Activity_Login.this , MainActivity.class);
                 startActivity( intent );
-
             }
-                /*
-                String userID = signin_name.getText().toString();
-                String userPassword = signin_password.getText().toString();
-                if(userID.equals(""))
-                {
-                    Toast.makeText(Activity_Login.this, "please fill in the e-mail", Toast.LENGTH_SHORT).show();
-                    signin_name.requestFocus();
-                }
-                else if(userPassword.equals(""))
-                {
-                    Toast.makeText(Activity_Login.this, "please fill in the password", Toast.LENGTH_SHORT).show();
-                    signin_password.requestFocus();
-                }
-                else {
-                    //SendDataToServer 사용하는 구간
-                    SendDataToServerSignInCheck(v);
-                    //SendDataToServer사용하는 구간 여기까지
-                    if(responseCode == 1)
-                    {
-                        //로그인 성공
-                        Toast.makeText(Activity_Login.this, "SignIn!", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(Activity_Login.this, MainActivity.class);
-                        //이 Activity에서 MainActivity로 값을 넘겨줌
-                        FirstscreenActivity.seq_num = seq_num;
-                        startActivity(intent);
-                    }
-                    else
-                    {
-                        //로그인 실패
-                        Toast.makeText(Activity_Login.this, "Incorrect User Information", Toast.LENGTH_SHORT).show();
-                    }
-
-                }
-            }
-
-        });*/
+        });
 
     }
 
